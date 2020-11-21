262e3275d7c5629807d94f4f6940c8cc91918a59
LexRed <44043211+LexRed@users.noreply.github.com>
2020-11-21 16:14:02 -0300

    registros

--- EscolaBD Script Final.sql
+++ EscolaBD Script Final.sql
@@ -11,30 +11,23 @@ create table pessoa (
     endereco varchar(45) not null,
     estadoCivil varchar(45) not null,
     nascimento date);
-
 create table disciplina (
 	idDisciplina int primary key not null,
     idturma int not null,
     matricula int 
 );
-
-
 create table professor (
     formacao varchar(45) not null,
     idDisciplina varchar(45) not null,
     turma varchar(1) not null);
-
 create table cargo (
 	idcargo int primary key not null,
     nome varchar(45) not null,
     funcao varchar(45) not null);
-    
-    
 create table aluno( 
 	serie int not null,
     periodo varchar(45) not null
 );
-
 create table turma (
 	idturma int not null primary key,
     turno varchar(45)
@@ -44,14 +37,12 @@ create table disciplina (
     idturma int not null,
     matricula int 
 );
-
 create table avisos (
 	idAviso int not null,
     dataAviso date,
     matricula varchar(40), /*id professor remetente da mensagem*/
     CPFresponsalvel varchar(40) /*destinatario da mensagem*/
 );
-
 create table responsavel (
 	filhos int not null
 );
@@ -62,7 +53,6 @@ create table avaliacao (
     idturma int not null,
     notaAvialicao int
 );
-
 create table tempoEstudo (
 	idDisciplina int not null,
     matricula int not null, /*aluno*/
@@ -74,3 +64,22 @@ create table notafinal (
     matricula int, /*aluno*/
     nota int
 );
+
+insert into pessoa (CPF, matricula, nome, telefone, endereco, estadoCivil, nascimento) values ('01', '001', 'Marcella', 'rua 01 casa 159', '1111', 'casada', '1970-05-09');
+insert into pessoa (CPF, matricula, nome, telefone, endereco, estadoCivil, nascimento) values ('02', '002', 'Mauricio', 'rua 02 casa 198', '1112', 'casado', '1988-03-01');
+insert into pessoa (CPF, matricula, nome, telefone, endereco, estadoCivil, nascimento) values ('03', '003', 'Denilson', 'rua 03 casa 155', '1113', 'casado', '1982-04-03');
+insert into pessoa (CPF, matricula, nome, telefone, endereco, estadoCivil, nascimento) values ('04', '004', 'Julianna', 'rua 04 casa 199', '1114', 'casada', '1988-09-05');
+insert into pessoa (CPF, matricula, nome, telefone, endereco, estadoCivil, nascimento) values ('05', '005', 'Fabricio', 'rua 05 casa 200', '1115', 'solteiro', '1988-08-22');
+
+insert into disciplina (idDisciplina, idturma, matricula) values ('1', '1', '001');
+insert into disciplina (idDisciplina, idturma, matricula) values ('2', '2', '002');
+insert into disciplina (idDisciplina, idturma, matricula) values ('3', '3', '003');
+insert into disciplina (idDisciplina, idturma, matricula) values ('4', '4', '004');
+insert into disciplina (idDisciplina, idturma, matricula) values ('5', '5', '005');
+
+insert into professor (formacao, idDisciplina, turma) values ('matematica', '1', 'A');
+insert into professor (formacao, idDisciplina, turma) values ('geografia', '2', 'A');
+insert into professor (formacao, idDisciplina, turma) values ('fisica', '3', 'A');
+insert into professor (formacao, idDisciplina, turma) values ('quimica', '4', 'A');
+insert into professor (formacao, idDisciplina, turma) values ('biologia', '5', 'A');
+
